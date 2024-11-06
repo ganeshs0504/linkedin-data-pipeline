8759df99550016de8bc7f3a2540d4f886d2179e9
ganeshs0504 <145580150+ganeshs0504@users.noreply.github.com>
2024-11-06 17:22:39 +0000

    Code quality (#5)
    * Added data loading to gcs with placeholder for next task
    
    * Added csv to parquet conversion with chunky gcs upload
    
    * Added transfomration, modelling, and BQ loading
    
    * Warehouse data model optimization for lower query cost
    
    * Code clean up and config file for both terraform and airflow dags

--- dags/linkedin_pipeline_dag.py
+++ dags/linkedin_pipeline_dag.py
@@ -1,23 +1,24 @@
 import os
 from scripts import sandbox
+from datetime import datetime
+import json
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.operators.python import BranchPythonOperator
-from airflow.operators.bash import BashOperator
 from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
-from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitPySparkJobOperator
 from airflow.operators.dummy import DummyOperator
 
-from datetime import datetime, timedelta
+with open("./config.json", 'r') as f:
+    config = json.load(f)
 
 default_args = {
     'owner': 'airflow',
 }
 
 with DAG(
-    dag_id='linkedin_datapipeline_dag_skill_arr',
+    dag_id='linkedin_datapipeline_dag_test_final',
     default_args=default_args,
     description='Something to say about this dag',
     # start_date=datetime(2024, 10, 22),
@@ -34,14 +35,6 @@ with DAG(
         python_callable=sandbox.check_if_file_exists_in_gcs
     )
 
-    # upload_file_to_gcs = LocalFilesystemToGCSOperator(
-    #     gcp_conn_id='gcp_creds',
-    #     task_id='upload_file_to_gcs',
-    #     src='raw_data/downloaded_data/',
-    #     dst='downloaded_data/',
-    #     bucket='test_bucket_airflow_99'
-    # )
-
     upload_folder_to_gcs = PythonOperator(
         task_id='upload_folder_to_gcs',
         python_callable=sandbox.upload_folder_to_gcs,
@@ -49,48 +42,44 @@ with DAG(
     )
 
     upload_dataproc_job_csv_to_parquet = LocalFilesystemToGCSOperator(
-        gcp_conn_id='gcp_creds',
+        gcp_conn_id=config["airflow_gcp_conn_id"],
         task_id='upload_dataproc_job_csv_to_parquet',
         src='dags/scripts/csv_to_parquet_pyspark.py',
         dst='csv_to_parquet_pyspark.py',
-        bucket='gcs-linkedin-dataproc-jobs'
+        bucket=config["dataproc_jobs_bucket_name"]
     )
 
     upload_dataproc_job_transform_to_bq = LocalFilesystemToGCSOperator(
-        gcp_conn_id='gcp_creds',
+        gcp_conn_id=config["airflow_gcp_conn_id"],
         task_id='upload_dataproc_job_transform_to_bq',
         src='dags/scripts/data_transformation_pyspark.py',
         dst='data_transformation_pyspark.py',
-        bucket='gcs-linkedin-dataproc-jobs'
+        bucket=config["dataproc_jobs_bucket_name"]
     )
 
     skip_upload_dummy = DummyOperator(task_id='skip_upload_dummy')
 
-    # PYSPARK_JOB = {
-    #     "placement": {"cluster_name": 'dataproc'}
-    # }
-
     submit_csv_to_parquet_dataproc_job = DataprocSubmitJobOperator(
         task_id='submit_csv_to_parquet_dataproc_job',
         job={
-            "placement": {"cluster_name": 'dataproc-cluster'},
-            "pyspark_job": {"main_python_file_uri": "gs://gcs-linkedin-dataproc-jobs/csv_to_parquet_pyspark.py"}
+            "placement": {"cluster_name": config["dataproc_cluster_name"]},
+            "pyspark_job": {"main_python_file_uri": f"gs://{config['dataproc_jobs_bucket_name']}/csv_to_parquet_pyspark.py"}
         },
         region="europe-west2",
         trigger_rule='all_done',
-        gcp_conn_id='gcp_creds'
+        gcp_conn_id=config["airflow_gcp_conn_id"]
     )
 
     transform_data_to_bq_dataproc_job = DataprocSubmitJobOperator(
         task_id='transform_data_to_bq_dataproc_job',
         job={
-            "placement": {"cluster_name": "dataproc-cluster"},
-            "pyspark_job": {"main_python_file_uri": "gs://gcs-linkedin-dataproc-jobs/data_transformation_pyspark.py",
+            "placement": {"cluster_name": config["dataproc_cluster_name"]},
+            "pyspark_job": {"main_python_file_uri": f"gs://{config['dataproc_jobs_bucket_name']}/data_transformation_pyspark.py",
                             "jar_file_uris": ['gs://spark-lib/bigquery/spark-3.1-bigquery-0.41.0.jar']
             }
         },
         region="europe-west2",
-        gcp_conn_id="gcp_creds"
+        gcp_conn_id=config["airflow_gcp_conn_id"]
     )
 
 
